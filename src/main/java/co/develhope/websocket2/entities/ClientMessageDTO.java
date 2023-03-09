bbfadf51273ee4a2a81688f14e5f1021c6549c6f
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2023-03-09 19:02:24 +0100

    Initial commit

---
+++ src/main/java/co/develhope/websocket2/entities/ClientMessageDTO.java
@@ -0,0 +1,40 @@
+package co.develhope.websocket2.entities;
+
+public class ClientMessageDTO {
+
+    private String clientName;
+    private String clientAlert;
+    private String clientMsg;
+
+    public ClientMessageDTO(String clientName, String clientAlert, String clientMsg) {
+        this.clientName = clientName;
+        this.clientAlert = clientAlert;
+        this.clientMsg = clientMsg;
+    }
+
+    public ClientMessageDTO(){}
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientAlert() {
+        return clientAlert;
+    }
+
+    public void setClientAlert(String clientAlert) {
+        this.clientAlert = clientAlert;
+    }
+
+    public String getClientMsg() {
+        return clientMsg;
+    }
+
+    public void setClientMsg(String clientMsg) {
+        this.clientMsg = clientMsg;
+    }
+}
